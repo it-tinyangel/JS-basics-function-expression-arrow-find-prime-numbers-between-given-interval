5e99fc603e12c0ab93e06ff967790e929b152562
likes-front-end <137172921+it-tinyangel@users.noreply.github.com>
2023-09-20 21:18:22 +0300

    finding prime numbers within in interval

---
+++ script.js
@@ -0,0 +1,43 @@
+const primeNumber = num => {
+	if (num <= 1) return false; // prime numbers greater than 1
+	if (num <= 3) return true; // 2 and 3 are primes
+	if (num % 2 === 0 || num % 3 === 0) return false; // checking if num is a multiple of 2 or 3
+
+	let i = 5;
+	while (i * i <= num) { // iteration while i squared <= number
+		if (num % i === 0 || num % (i + 2) === 0) return false; // num isn't prime
+		i += 6; // for numbers 6k ± 1, where k is an integer
+	}
+
+	return true; // num is a prime
+};
+
+const simpleNumber = () => {
+
+	const startInputNumber = parseInt(prompt('Enter the start of interval:'));
+	const endInputNumber = parseInt(prompt('Enter the end of interval:'));
+
+	if (isNaN(startInputNumber) || isNaN(endInputNumber) || startInputNumber === null || endInputNumber === null || startInputNumber === "" || endInputNumber === "" || startInputNumber > endInputNumber) {
+		alert('You entered an incorrect value. Please, try again.'); // error message
+		return;
+	}
+
+	// correctly entered values is converted into numbers:
+	const start = Number(startInputNumber);
+	const end = Number(endInputNumber);
+
+	let primeNumbersString = ''; // to store prime numbers as a string
+
+	for (let i = start; i <= end; i++) { // iteration within an interval
+		if (primeNumber(i)) {
+			if (primeNumbersString !== '') {
+				primeNumbersString += ', '; // adding a comma if it's not first number
+			}
+			primeNumbersString += i; // adding a prime number to the string
+		}
+	}
+
+	console.log(`Prime numbers in interval [${start}, ${end}]: ${primeNumbersString}`);
+};
+
+simpleNumber();
